88fd4c8e0d0e0e7c91e7029ca6070510315560f5
Lee Iverson <leei@ece.ubc.ca>
2011-05-09 18:55:01 -0700

    Update vec.cc for new IntVec impl

--- vec.cc
+++ vec.cc
@@ -7,67 +7,10 @@
 #include <node.h>
 
 #include "bitvec.h"
+#include "intvec.h"
 
 using namespace node;
 using namespace v8;
-
-class IntVec: ObjectWrap
-{
-private:
-  int length;
-public:
-
-  static Persistent<FunctionTemplate> s_ct;
-  static void Init(Handle<Object> target)
-  {
-    HandleScope scope;
-
-    Local<FunctionTemplate> t = FunctionTemplate::New(New);
-
-    s_ct = Persistent<FunctionTemplate>::New(t);
-    s_ct->InstanceTemplate()->SetInternalFieldCount(1);
-    s_ct->SetClassName(String::NewSymbol("IntVec"));
-
-    NODE_SET_PROTOTYPE_METHOD(s_ct, "hello", Hello);
-
-    target->Set(String::NewSymbol("IntVec"),
-                s_ct->GetFunction());
-  }
-
-  IntVec() : length(0)
-  {
-  }
-
-  ~IntVec()
-  {
-  }
-
-  static Handle<Value> New(const Arguments& args)
-  {
-    HandleScope scope;
-    IntVec* hw = new IntVec();
-    hw->Wrap(args.This());
-    return args.This();
-  }
-
-  static Handle<Value> Hello(const Arguments& args)
-  {
-    HandleScope scope;
-    //IntVec* hw = ObjectWrap::Unwrap<IntVec>(args.This());
-    Local<String> result = String::New("Hello World");
-    return scope.Close(result);
-  }
-
-  static Handle<Value> GetLength(const Arguments& args)
-  {
-    HandleScope scope;
-    IntVec* hw = ObjectWrap::Unwrap<IntVec>(args.This());
-    return scope.Close(Integer::New(hw->length));
-  }
-
-};
-
-Persistent<FunctionTemplate> IntVec::s_ct;
 
 extern "C" {
   static void init (Handle<Object> target)
